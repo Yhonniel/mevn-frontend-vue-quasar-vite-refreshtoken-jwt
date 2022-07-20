d9538ec579a267cac136b68c6a0c54368591f476
Yhonniel Javier Suárez Bellorin <yhs@iMac-de-Yhonniel.local>
2022-07-19 20:22:37 -0500

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-page padding>
+    <q-btn @click="access">INGRESAR</q-btn>
+    <q-btn @click="createLink">Crear Link</q-btn>
+    {{ token }} - {{ expiresIn }}
+  </q-page>
+</template>
+
+<script setup>
+//import axios from "axios";
+import { api } from "src/boot/axios";
+import { ref } from "vue";
+
+// const access = () => {
+//   console.log("access");
+//   axios
+//     .post("http://localhost:3001/api/v1/auth/login", {
+//       email: "rigo@test.com",
+//       password: "123123",
+//     })
+//     .then((res) => {
+//       console.log(res.data);
+//     })
+//     .catch((e) => console.log(e));
+// };
+
+const token = ref("");
+const expiresIn = ref("");
+
+const access = async () => {
+  try {
+    const res = await api.post("/auth/login", {
+      email: "rigo@test.com",
+      password: "123123",
+    });
+    token.value = res.data.token;
+    expiresIn.value = res.data.expiresIn;
+    setTime();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createLink = async () => {
+  try {
+    const res = await api({
+      method: "POST",
+      url: "/links",
+      headers: {
+        Authorization: "Bearer " + token.value,
+      },
+      data: {
+        longLink: "https://www.google.com",
+      },
+    });
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const setTime = () => {
+  setTimeout(() => {
+    console.log("Se refrescó");
+    refreshToken();
+  }, expiresIn.value * 1000 - 6000);
+};
+
+const refreshToken = async () => {
+  try {
+    const res = await api.get("/auth/refresh");
+    token.value = res.data.token;
+    expiresIn.value = res.data.expiresIn;
+    setTime();
+  } catch (error) {
+    console.log(error);
+  }
+};
+refreshToken();
+</script>
